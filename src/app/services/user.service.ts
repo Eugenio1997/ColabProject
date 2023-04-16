19141b94c35679c99b0e0b4c6065e09bfeecf833
Eugenio <eugeniolimaeugenio5000@outlook.com>
2023-04-16 18:46:48 -0300

    hotfix/# Corrigindo a baseURL

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,12 +5,12 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class UserService {
 
-  private baseURL: string = 'https://randomuser.me/api/' + "?results=100";
+  private baseURL: string = 'https://randomuser.me/api/';
 
   constructor(private http: HttpClient) { }
 
   public getApiUsers():Observable<any>{
-    return this.http.get(this.baseURL);
+    return this.http.get(this.baseURL + "?results=100");
   }
 
 }
